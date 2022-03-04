78682cc65543cde66ed69b36d992af20ac0ea30c
Michael-Andreas <michaelandreas246@gmail.com>
2022-03-04 14:05:08 +0100

    Searchbar styled

--- src/component/Searchbar.jsx
+++ src/component/Searchbar.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
-export default function Searchbar({handleClick, userInput, setUserInput}) {
+export default function Searchbar({ handleClick, userInput, setUserInput }) {
   return (
     <div>
       <input
-        className="searchInput"
+        className="searchInput form-control rounded"
         type="text"
         placeholder="Search..."
         value={userInput}
         onChange={(e) => setUserInput(e.target.value)}
       />
-      <button onClick={handleClick} className="searchBtn">
+      <button
+        onClick={handleClick}
+        className="searchBtn btn btn-outline-primary"
+      >
         Search
       </button>
-      ;
     </div>
   );
 }
